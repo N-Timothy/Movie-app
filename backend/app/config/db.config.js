d546c0c85076e63b02ebba6b74230a61adf9651a
Nathanael <timmynathan10@gmail.com>
2022-06-01 15:16:04 +0700

    movie app backend

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "nathanael",
-    PASSWORD: "Nathanael10.",
-    DB: "movie_database"
+    // HOST: "localhost",
+    // USER: "nathanael",
+    // PASSWORD: "Nathanael10.",
+    // DB: "movie_database"
+
+    HOST: "sql6.freemysqlhosting.net",
+    USER: "sql6496973",
+    PASSWORD: "6VPdUQrpcM",
+    DB: "sql6496973"
 };
